dfc4d86acbadcd0b98a2a1c15a23b0ff995147b0
joshtsen <tsen.joshua@gmail.com>
2015-05-08 22:14:35 -0700

    Modified keymap

---
+++ keyboard/gh60/keymap_gamepad.c
@@ -0,0 +1,19 @@
+#include "keymap_common.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    KEYMAP(MINS, FN0,  1, 2, 3, 4, 5, MPLY, \
+           EQL,  TAB,  Q, W, E, R, T, SCLN, \
+                 LCTL, A, S, D, F, G, QUOT, \
+           COMM, LSFT,  Z,   C, V, B, \
+           SLSH, LALT, X, DOT, SPC),
+    
+    KEYMAP(TRNS, TRNS, F1, F2, F3, F4, F5, TRNS, \
+           TRNS, TRNS, F6, F7, F8, F9, F10, TRNS, \
+                 TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
+           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
+           TRNS, TRNS, TRNS, TRNS, TRNS),
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+  [0] ACTION_LAYER_TAP_KEY(1, KC_ESC),
+};

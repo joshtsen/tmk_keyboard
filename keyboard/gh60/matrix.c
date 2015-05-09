7c0a5e3b3b984e7eb27e658d679855cd908bf5a5
joshtsen <tsen.joshua@gmail.com>
2015-05-09 13:02:01 -0700

    Reveresed binary in unselect_rows

--- keyboard/gh60/matrix.c
+++ keyboard/gh60/matrix.c
@@ -167,8 +167,8 @@ static matrix_row_t read_cols(void)
 static void unselect_rows(void)
 {
     // Hi-Z(DDR:0, PORT:0) to unselect
-    DDRB  &= ~0b11110001;
-    PORTB &= ~0b11110001;
+    DDRB  &= ~0b10001111;
+    PORTB &= ~0b10001111;
 }
 
 static void select_row(uint8_t row)
